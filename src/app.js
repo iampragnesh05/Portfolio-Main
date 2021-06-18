0f03e2a3616dd7e611c39a54242b1fad3184faeb
Pragnesh Shrimal <iampragnesh05@gmail.com>
2021-06-18 16:08:57 +0530

    Setup add for Heroku

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const path = require('path');
-const port = 3000;
+const port = process.env.PORT || 3000;
 const _ = require('lodash');
 
 const app = express();
